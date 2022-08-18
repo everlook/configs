e0b86ea64cf55ad1583898c805c5209d6ff07b27
Jeff Horn <outforaride@gmail.com>
2022-08-18 14:35:38 -0700

    add telescope symbols keymap
    Signed-off-by: Jeff Horn <outforaride@gmail.com>

--- nvim-lua/lua/everlook/plugins/telescope.lua
+++ nvim-lua/lua/everlook/plugins/telescope.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<leader>ff", "<cmd>lua require'telescope.builtin'.find_files()<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>lua require'telescope.builtin'.buffers()<cr>")
+vim.keymap.set("n", "<leader>fs", "<cmd>Telescope lsp_document_symbols<cr>")
